ce414ec25d6bba2cf0cd133e96155f29bcf51f19
AshersLab <Asher.parry@icloud.com>
2020-04-21 23:55:35 +1000

    Anchor Pos Warning (#125)
    Adds a warning and stops scan saving if Anchor Pos is set outside schematic to reduce issues

--- src/api/java/com/ldtteam/structurize/api/util/constant/TranslationConstants.java
+++ src/api/java/com/ldtteam/structurize/api/util/constant/TranslationConstants.java
@@ -10,6 +10,8 @@ public final class TranslationConstants
     @NonNls
     public static final String MAX_SCHEMATIC_SIZE_REACHED = "item.sceptersteel.toobig";
 
+    public static final String ANCHOR_POS_OUTSIDE_SCHEMATIC = "item.sceptersteel.badanchorpos";
+
     private TranslationConstants()
     {
         //empty default
--- src/main/java/com/ldtteam/structurize/items/ItemScanTool.java
+++ src/main/java/com/ldtteam/structurize/items/ItemScanTool.java
@@ -28,6 +28,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static com.ldtteam.structurize.api.util.constant.Constants.MAX_SCHEMATIC_SIZE;
+import static com.ldtteam.structurize.api.util.constant.TranslationConstants.ANCHOR_POS_OUTSIDE_SCHEMATIC;
 import static com.ldtteam.structurize.api.util.constant.TranslationConstants.MAX_SCHEMATIC_SIZE_REACHED;
 
 /**
@@ -120,6 +121,30 @@ public class ItemScanTool extends AbstractItemWithPosSelector
             final boolean saveEntities,
             final Optional<BlockPos> anchorPos)
     {
+        if (anchorPos.isPresent())
+        {
+            final double minX = Math.min(to.getX(), from.getX());
+            final double minY = Math.min(to.getY(), from.getY());
+            final double minZ = Math.min(to.getZ(), from.getZ());
+            final double maxX = Math.max(to.getX(), from.getX());
+            final double maxY = Math.max(to.getY(), from.getY());
+            final double maxZ = Math.max(to.getZ(), from.getZ());
+
+            final double anchorPosX = anchorPos.get().getX();
+            final double anchorPosY = anchorPos.get().getY();
+            final double anchorPosZ = anchorPos.get().getZ();
+
+            final boolean isWithinX = anchorPosX >= minX && anchorPosX <= maxX;
+            final boolean isWithinY = anchorPosY >= minY && anchorPosY <= maxY;
+            final boolean isWithinZ = anchorPosZ >= minZ && anchorPosZ <= maxZ;
+
+            if (!(isWithinX && isWithinY && isWithinZ))
+            {
+                LanguageHandler.sendPlayerMessage(player, ANCHOR_POS_OUTSIDE_SCHEMATIC);
+                return;
+            }
+        }
+
         final BlockPos blockpos =
           new BlockPos(Math.min(from.getX(), to.getX()), Math.min(from.getY(), to.getY()), Math.min(from.getZ(), to.getZ()));
         final BlockPos blockpos1 =
