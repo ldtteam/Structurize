1e49650b64ea0a7dd043284b46345cfd5e5bd24d
Mark E. Schill <Mark.Schill@cmschill.net>
2020-06-13 06:43:05 -0400

    Prevents blocks from connecting to the substitution block
    Closes #

--- src/main/java/com/ldtteam/structurize/blocks/schematic/BlockSubstitution.java
+++ src/main/java/com/ldtteam/structurize/blocks/schematic/BlockSubstitution.java
@@ -1,12 +1,17 @@
 package com.ldtteam.structurize.blocks.schematic;
 
 import com.ldtteam.structurize.blocks.AbstractBlockStructurize;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.world.IBlockReader;
 
 /**
- * This block is used as a substitution block for the Builder.
- * Every solid block can be substituted by this block in schematics.
- * This helps make schematics independent from location and ground.
+ * This block is used as a substitution block for the Builder. Every solid block can be substituted by this block in schematics. This helps make schematics independent from
+ * location and ground.
  */
 public class BlockSubstitution extends AbstractBlockStructurize<BlockSubstitution>
 {
@@ -27,12 +32,17 @@ public class BlockSubstitution extends AbstractBlockStructurize<BlockSubstitutio
     private static final float RESISTANCE = 1F;
 
     /**
-     * Constructor for the Substitution block.
-     * sets the creative tab, as well as the resistance and the hardness.
+     * Constructor for the Substitution block. sets the creative tab, as well as the resistance and the hardness.
      */
     public BlockSubstitution()
     {
         super(Properties.create(Material.WOOD).hardnessAndResistance(BLOCK_HARDNESS, RESISTANCE));
         setRegistryName(BLOCK_NAME);
+    }
+
+    @Override
+    public VoxelShape getShape(final BlockState state, final IBlockReader worldIn, final BlockPos pos, final ISelectionContext context)
+    {
+        return VoxelShapes.create(.125D, .125D, .125D, .875D, .875D, .875D);
     }
 }
