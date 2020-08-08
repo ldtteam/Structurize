d5cb9845b1782d8a0f1257dcbb8733fc406a2ffe
Nightenom <17338378+Nightenom@users.noreply.github.com>
2020-08-08 12:56:35 +0200

    Another optifine fix (#197)
    - make some things to make optifine compatibility more stable, most shaders work now correctly
    - added config value to disable optifine compatibility completely

--- src/main/java/com/ldtteam/structurize/config/ClientConfiguration.java
+++ src/main/java/com/ldtteam/structurize/config/ClientConfiguration.java
@@ -9,11 +9,21 @@ import net.minecraftforge.common.ForgeConfigSpec;
 public class ClientConfiguration extends AbstractConfiguration
 {
     /**
+     * Use Optifine Compat Patch. If false OptifineCompat will never initialize.
+     */
+    public final ForgeConfigSpec.BooleanValue useOptifineCompatPatch;
+
+    /**
      * Builds client configuration.
      *
      * @param builder config builder
      */
     protected ClientConfiguration(final ForgeConfigSpec.Builder builder)
     {
+        createCategory(builder, "optifine");
+
+        useOptifineCompatPatch = defineBoolean(builder, "useOptifineCompatPatch", true);
+
+        finishCategory(builder);
     }
 }
