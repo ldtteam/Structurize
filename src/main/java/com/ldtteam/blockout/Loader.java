ea66e031a85fd88022129187ed419063ecbc5340
Nightenom <17338378+Nightenom@users.noreply.github.com>
2020-12-25 15:04:37 +0100

    Reduce a bit of lag during xml parsing

--- src/main/java/com/ldtteam/blockout/Loader.java
+++ src/main/java/com/ldtteam/blockout/Loader.java
@@ -27,6 +27,8 @@ import javax.xml.parsers.ParserConfigurationException;
  */
 public final class Loader
 {
+    private static final DocumentBuilderFactory DOCUMENT_BUILDER_FACTORY = DocumentBuilderFactory.newInstance();
+
     private static final Map<String, Constructor<? extends Pane>> paneConstructorMap = new HashMap<>();
     static
     {
@@ -180,8 +182,7 @@ public final class Loader
     {
         try
         {
-            final DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            final DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            final DocumentBuilder dBuilder = DOCUMENT_BUILDER_FACTORY.newDocumentBuilder();
             final Document doc = dBuilder.parse(input);
             input.getByteStream().close();
 
