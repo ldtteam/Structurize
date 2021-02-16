4ae648cd518335b5eeb5b714817cdcaf5fe8de1d
Nightenom <17338378+Nightenom@users.noreply.github.com>
2021-02-16 14:16:08 +0100

    Upgrade datagen to be sorted

--- src/main/java/com/ldtteam/structurize/generation/timber_frames/TimberFramesLangEntryProvider.java
+++ src/main/java/com/ldtteam/structurize/generation/timber_frames/TimberFramesLangEntryProvider.java
@@ -1,44 +1,26 @@
 package com.ldtteam.structurize.generation.timber_frames;
 
-import com.google.gson.JsonParser;
 import com.ldtteam.datagenerators.lang.LangJson;
 import com.ldtteam.structurize.blocks.ModBlocks;
 import com.ldtteam.structurize.blocks.decorative.BlockTimberFrame;
-import com.ldtteam.structurize.generation.DataGeneratorConstants;
-import net.minecraft.data.DataGenerator;
 import net.minecraft.data.DirectoryCache;
 import net.minecraft.data.IDataProvider;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
-import java.nio.file.Path;
 
 public class TimberFramesLangEntryProvider implements IDataProvider
 {
-    private final DataGenerator generator;
+    private final LangJson backingLangJson;
 
-    public TimberFramesLangEntryProvider(DataGenerator generator)
+    public TimberFramesLangEntryProvider(LangJson backingLangJson)
     {
-        this.generator = generator;
+        this.backingLangJson = backingLangJson;
     }
 
     @Override
     public void act(@NotNull DirectoryCache cache) throws IOException
     {
-        final Path inputPath = generator.getInputFolders().stream().findFirst().orElse(null);
-
-        if (inputPath == null)
-            return;
-
-        final File langFile = inputPath.resolve(DataGeneratorConstants.EN_US_LANG).toFile();
-        final Reader reader = new FileReader(langFile);
-
-        final LangJson langJson = new LangJson();
-        langJson.deserialize(new JsonParser().parse(reader));
-
         for (BlockTimberFrame timberFrame : ModBlocks.getTimberFrames())
         {
             if (timberFrame.getRegistryName() == null)
@@ -47,11 +29,8 @@ public class TimberFramesLangEntryProvider implements IDataProvider
             final String reference = "block.structurize." + timberFrame.getRegistryName().getPath();
             final String value = timberFrame.getTimberFrameType().getLangName() + " " + timberFrame.getFrameType().getLangName() + " " + timberFrame.getCentreType().getLangName() + " Timber Frame";
 
-            langJson.getLang().put(reference, value);
+            backingLangJson.put(reference, value);
         }
-
-        IDataProvider.save(DataGeneratorConstants.GSONLang, cache, langJson.serialize(), langFile.toPath());
-
     }
 
     @Override
