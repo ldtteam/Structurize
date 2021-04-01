bb2b4d69da44f8842cae8b374220eb90f79c61bc
Jay Bell <30493222+ShadowProtocol@users.noreply.github.com>
2021-04-01 21:26:16 +1100

    Allow barrels to be waterlogged

--- src/main/java/com/ldtteam/structurize/blocks/decorative/BlockBarrel.java
+++ src/main/java/com/ldtteam/structurize/blocks/decorative/BlockBarrel.java
@@ -3,15 +3,21 @@ package com.ldtteam.structurize.blocks.decorative;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.IWaterLoggable;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.Direction;
 import net.minecraft.util.Mirror;
 import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
 import org.jetbrains.annotations.Nullable;
 
 import static net.minecraft.state.properties.BlockStateProperties.HORIZONTAL_FACING;
@@ -19,22 +25,25 @@ import static net.minecraft.state.properties.BlockStateProperties.HORIZONTAL_FAC
 /**
  * Decorative barrel block
  */
-public class BlockBarrel extends Block
+public class BlockBarrel extends Block implements IWaterLoggable
 {
     /**
      * This Blocks shape.
      */
     protected static final VoxelShape SHAPE = Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 15.0D, 15.0D, 15.0D);
 
+	public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+
     public BlockBarrel()
     {
         super(Block.Properties.from(Blocks.OAK_PLANKS).hardnessAndResistance(3f, 1f));
+        this.setDefaultState(this.getStateContainer().getBaseState().with(WATERLOGGED, false));
     }
 
     @Override
     protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder)
     {
-        builder.add(BlockStateProperties.HORIZONTAL_FACING);
+        builder.add(BlockStateProperties.HORIZONTAL_FACING, WATERLOGGED);
     }
 
     @Override
@@ -59,6 +68,22 @@ public class BlockBarrel extends Block
     @Override
     public BlockState getStateForPlacement(final BlockItemUseContext context)
     {
-        return super.getStateForPlacement(context).with(HORIZONTAL_FACING, context.getPlacementHorizontalFacing());
+	    FluidState fluidstate = context.getWorld().getFluidState(context.getPos());
+        return super.getStateForPlacement(context).with(HORIZONTAL_FACING, context.getPlacementHorizontalFacing()).with(WATERLOGGED, fluidstate.getFluid() == Fluids.WATER);
     }
+
+	public FluidState getFluidState(BlockState state)
+	{
+		return state.get(WATERLOGGED) ? Fluids.WATER.getStillFluidState(false) : super.getFluidState(state);
+	}
+
+	public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos)
+	{
+		if (stateIn.get(WATERLOGGED))
+		{
+			worldIn.getPendingFluidTicks().scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickRate(worldIn));
+		}
+
+		return stateIn;
+	}
 }
