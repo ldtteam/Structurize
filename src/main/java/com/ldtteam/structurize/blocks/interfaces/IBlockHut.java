589d20897321de6b1d30390b699769f61187fa0f
Justen-N <losthalflingrogue@gmail.com>
2021-07-06 08:56:24 -0600

    Add BlockHut interface and swap check to IBlockHut, remove unused List.

---
+++ src/main/java/com/ldtteam/structurize/blocks/interfaces/IBlockHut.java
@@ -0,0 +1,9 @@
+package com.ldtteam.structurize.blocks.interfaces;
+/*
+ * Interface for all Blocks which are considered 'hut blocks'
+ */
+public interface IBlockHut {
+    /*
+     *No current methods.
+     */
+}
