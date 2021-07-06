d751989051c238a20de44fc172f60c27cd6da4ef
Justen-N <losthalflingrogue@gmail.com>
2021-07-06 15:43:54 -0600

    Revert "Add BlockHut interface and swap check to IBlockHut, remove unused List."
    This reverts commit 589d2089

--- src/main/java/com/ldtteam/structurize/blocks/interfaces/IBlockHut.java
+++
@@ -1,9 +0,0 @@
-package com.ldtteam.structurize.blocks.interfaces;
-/*
- * Interface for all Blocks which are considered 'hut blocks'
- */
-public interface IBlockHut {
-    /*
-     *No current methods.
-     */
-}
