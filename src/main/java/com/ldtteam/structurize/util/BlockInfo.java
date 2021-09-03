9fe18e123131139b49d07ad73ec85c1c2df6fa64
Nightenom <17338378+Nightenom@users.noreply.github.com>
2021-09-03 12:27:07 +0200

    Records

--- src/main/java/com/ldtteam/structurize/util/BlockInfo.java
+++ src/main/java/com/ldtteam/structurize/util/BlockInfo.java
@@ -8,58 +8,10 @@ import org.jetbrains.annotations.Nullable;
 /**
  * Contains block information we need for placement.
  */
-public class BlockInfo
+public record BlockInfo(BlockPos getPos, @Nullable BlockState getState, @Nullable CompoundTag getTileEntityData)
 {
-    /**
-     * The position in the structure (relative pos not world pos).
-     */
-    private final BlockPos pos;
-
-    /**
-     * The blockstate to be placed.
-     */
-    @Nullable
-    private final BlockState state;
-
-    /**
-     * The tileEntity information we need.
-     */
-    @Nullable
-    private final CompoundTag tileEntityData;
-
-    /**
-     * Creator of the block info class.
-     *
-     * @param pos            the position.
-     * @param state          the state.
-     * @param tileEntityData the tileEntity data.
-     */
-    public BlockInfo(final BlockPos pos, @Nullable final BlockState state, @Nullable final CompoundTag tileEntityData)
-    {
-        this.pos = pos;
-        this.state = state;
-        this.tileEntityData = tileEntityData;
-    }
-
-    public BlockPos getPos()
-    {
-        return pos;
-    }
-
-    @Nullable
-    public BlockState getState()
-    {
-        return state;
-    }
-
-    @Nullable
-    public CompoundTag getTileEntityData()
-    {
-        return tileEntityData;
-    }
-
     public boolean hasTileEntityData()
     {
-        return tileEntityData != null;
+        return getTileEntityData != null;
     }
 }
