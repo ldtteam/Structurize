9b2e3d5062fc46960f289d3368755be847a858d6
Ray  Neiheiser <ray.neiheiser@gmail.com>
2022-09-04 08:30:00 +0200

    bump and datagen fix

--- src/main/java/com/ldtteam/structurize/storage/ClientStructurePackLoader.java
+++ src/main/java/com/ldtteam/structurize/storage/ClientStructurePackLoader.java
@@ -69,6 +69,12 @@ public class ClientStructurePackLoader
             modList.add(mod.getModId());
         }
 
+        if (Minecraft.getInstance() == null)
+        {
+            // RunData
+            return;
+        }
+
         final Path gameFolder = Minecraft.getInstance().gameDirectory.toPath();
 
         IOPool.execute(() ->
