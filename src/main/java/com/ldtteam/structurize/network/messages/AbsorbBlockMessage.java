8ec59b3897e8bd52d07d9869c56871296d85a35a
Mirality <uecasm@users.noreply.github.com>
2022-10-09 17:20:46 +1300

    javadoc

--- src/main/java/com/ldtteam/structurize/network/messages/AbsorbBlockMessage.java
+++ src/main/java/com/ldtteam/structurize/network/messages/AbsorbBlockMessage.java
@@ -10,23 +10,39 @@ import net.minecraftforge.network.NetworkEvent;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * Sent client to server to request that the currently held item should "absorb" a new replacement block.
+ */
 public class AbsorbBlockMessage implements IMessage
 {
     private final BlockPos pos;
     private final ItemStack stack;
 
+    /**
+     * Construct
+     * @param pos the location of the block to absorb (in the sender's level)
+     * @param stack the "picked item" stack produced by that block
+     */
     public AbsorbBlockMessage(@NotNull final BlockPos pos, @NotNull final ItemStack stack)
     {
         this.pos = pos;
         this.stack = stack;
     }
 
+    /**
+     * Deserialize
+     * @param buf the network buffer
+     */
     public AbsorbBlockMessage(@NotNull final FriendlyByteBuf buf)
     {
         this.pos = buf.readBlockPos();
         this.stack = buf.readItem();
     }
 
+    /**
+     * Serialize
+     * @param buf network data byte buffer
+     */
     @Override
     public void toBytes(@NotNull final FriendlyByteBuf buf)
     {
