a540474647e6a27ce709bd1bb34bbe1df823c4dd
Gavin Lambert <uecasm@users.noreply.github.com>
2022-10-18 22:56:19 +1300

    Fix painting placement (#549)
    Painting placement properly positioned

--- src/main/java/com/ldtteam/structurize/placement/StructurePlacer.java
+++ src/main/java/com/ldtteam/structurize/placement/StructurePlacer.java
@@ -16,6 +16,7 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.decoration.HangingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
@@ -231,7 +232,11 @@ public class StructurePlacer
                             entity.deserializeNBT(compound);
 
                             entity.setUUID(UUID.randomUUID());
-                            final Vec3 posInWorld = entity.position().add(pos.getX(), pos.getY(), pos.getZ());
+                            Vec3 posInWorld = entity.position().add(pos.getX(), pos.getY(), pos.getZ());
+                            if (entity instanceof HangingEntity hang)
+                            {
+                                posInWorld = posInWorld.subtract(Vec3.atLowerCornerOf(hang.blockPosition().subtract(hang.getPos())));
+                            }
                             entity.moveTo(posInWorld.x, posInWorld.y, posInWorld.z, entity.getYRot(), entity.getXRot());
 
                             final List<? extends Entity> list = world.getEntitiesOfClass(entity.getClass(), new AABB(posInWorld.add(1,1,1), posInWorld.add(-1,-1,-1)));
