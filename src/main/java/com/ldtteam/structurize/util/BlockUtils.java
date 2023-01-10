49d9812a0a4ef880352dc7bde8137499452eeef9
Gavin Lambert <uecasm@users.noreply.github.com>
2023-01-10 20:15:54 +1300

    Different method to avoid popping neighbours (#562)
    Different implementation for Don't pop neighbours on replace #560 since that caused issues with DO blocks not replacing
    Note that DO blocks will still not replace properly with Structurize alone; needs placement handler currently implemented only in MineColonies too.
    Update the resource list after triggering replace
    This works in SP; not tested in MP but very likely to not work there due to racing packets. But no worse than existing behaviour.

--- src/main/java/com/ldtteam/structurize/util/BlockUtils.java
+++ src/main/java/com/ldtteam/structurize/util/BlockUtils.java
@@ -554,10 +554,7 @@ public final class BlockUtils
             }
 
             // place
-            if (blockNeedsRemoval(world, here))
-            {
-                world.removeBlock(here, false);
-            }
+            world.setBlock(here, Blocks.COBBLESTONE.defaultBlockState(), Block.UPDATE_NONE);
             world.setBlock(here, newState, Constants.UPDATE_FLAG);
             targetBlock.setPlacedBy(world, here, newState, fakePlayer, stackToPlace);
         }
@@ -578,10 +575,7 @@ public final class BlockUtils
             }
             else
             {
-                if (blockNeedsRemoval(world, here))
-                {
-                    world.removeBlock(here, false);
-                }
+                world.setBlock(here, Blocks.COBBLESTONE.defaultBlockState(), Block.UPDATE_NONE);
                 world.setBlock(here, fluid.defaultFluidState().createLegacyBlock(), Constants.UPDATE_FLAG);
                 bucket.checkExtraContent(null, world, stackToPlace, here);
             }
@@ -591,14 +585,6 @@ public final class BlockUtils
             throw new IllegalArgumentException(
                 MessageFormat.format("Cannot handle placing of {0} instead of {1}?!", itemStack.toString(), blockState.toString()));
         }
-    }
-
-    private static boolean blockNeedsRemoval(Level world, BlockPos pos)
-    {
-        final BlockState state = world.getBlockState(pos);
-        final Block block = state.getBlock();
-
-        return block instanceof DoorBlock || block instanceof DoublePlantBlock;
     }
 
     /**
