50a00f412574d081782cc3348999df654c095733
Raycoms <ray.neiheiser@gmail.com>
2023-07-09 09:36:05 +0200

    weh

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -46,7 +46,7 @@ project {
         param("env.Version.Minor", "0")
         param("Upsource.Project.Id", "structurize")
         param("Default.Branch", "version/main")
-        param("env.GRADLE_VERSION", "8.1.1")
+        param("env.GRADLE_VERSION", "7.3")
         param("filename.prefix", "structurize")
     }
 
@@ -79,7 +79,7 @@ object Beta : Project({
     params {
         text("env.crowdinKey", "credentialsJSON:57fbe4f4-13dd-4c72-b6b3-3cc1e3a8240e", label = "Crowdin key", description = "The API key for crowdin to pull translations", allowEmpty = true)
         param("Current Minecraft Version", "main")
-        param("env.GRADLE_VERSION", "8.1.1")
+        param("env.GRADLE_VERSION", "7.3")
         param("Default.Branch", "version/%Current Minecraft Version%")
         param("VCS.Branches", "+:refs/heads/version/(*)")
         param("env.CURSERELEASETYPE", "beta")
