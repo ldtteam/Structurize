56708e609108baeafa44bc369afbc9c12a2d123f
Raycoms <ray.neiheiser@gmail.com>
2023-08-04 21:41:20 +0200

    proper fix (#596)
    Closes ldtteam/minecolonies#9278
    Closes ldtteam/minecolonies#9255

--- src/api/java/com/ldtteam/structurize/api/util/Utils.java
+++ src/api/java/com/ldtteam/structurize/api/util/Utils.java
@@ -1,11 +1,14 @@
 package com.ldtteam.structurize.api.util;
 
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.player.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+import java.util.Objects;
 
 /**
  * General purpose utilities class.
@@ -48,5 +51,26 @@ public final class Utils
         {
             Log.getLogger().error("Directory doesn't exist and failed to be created: " + directory.toString());
         }
+    }
+
+    /**
+     * Check if a comparison tag has the same or more nbt tags than an origin nbt tag.
+     * @param originTag the base tag to start the comparison from.
+     * @param compareTag the tag we try to fit into the origin tag.
+     * @return true if they match.
+     */
+    public static boolean nbtContains(final CompoundTag originTag, final CompoundTag compareTag)
+    {
+        for (final String childTagKey : originTag.getAllKeys())
+        {
+            final Tag originChildTag = originTag.get(childTagKey);
+            final Tag compareChildTag = compareTag.get(childTagKey);
+
+            if (!Objects.equals(originChildTag, compareChildTag))
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
