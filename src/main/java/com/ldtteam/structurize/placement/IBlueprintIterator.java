fe73fa268c40d0d9c963791fca666ad3119dc749
MotionlessTrain <timothy@familie-de-moor.nl>
2024-06-16 16:43:38 +0200

    Chore: new Delegate blueprint iterator implementation (#670)
    Adds a new AbstractDelegateBlueprintIterator class, which can be used to create new BlueprintIterators based on other iterators,
    to e.g. apply extra logic to incrementing and decrementing, whilst not being dependent on a particular iterator type
    The logic in AbstractBlueprintIterator has been separated in an interface (which is implemented in the delegate iterator as well), a new abstract class (to share the iterator with skip condition logic), and the actual implementation
    The latter has been renamed into a new class AbstractStructureIterator, whose name has been inspired by the StructureIterators "registry" class. That is now the base class of the different iterator types
    This will be used by a rework of the quarrier in Minecolonies, which will use an approach with a "LayerBlueprintIterator", which iterates over a single Y-level and then finishes, using a delegate iterator which defines the actual iteration pattern for that layer

---
+++ src/main/java/com/ldtteam/structurize/placement/IBlueprintIterator.java
@@ -0,0 +1,91 @@
+package com.ldtteam.structurize.placement;
+
+import com.ldtteam.structurize.placement.structure.IStructureHandler;
+import com.ldtteam.structurize.util.BlueprintPositionInfo;
+import net.minecraft.core.BlockPos;
+import net.minecraftforge.common.util.TriPredicate;
+
+public interface IBlueprintIterator
+{
+
+    /**
+     * Increment the structure with a certain skipCondition (jump over blocks that fulfill skipCondition).
+     * @param skipCondition the skipCondition.
+     * @return Result of increment.
+     */
+    AbstractBlueprintIterator.Result increment(TriPredicate<BlueprintPositionInfo, BlockPos, IStructureHandler> skipCondition);
+
+    /**
+     * Increment method, create in implementation.
+     *
+     * @return increment the structure position (y++).
+     */
+    AbstractBlueprintIterator.Result increment();
+
+    /**
+     * Decrement method, create in implementation.
+     *
+     * @return decrement the structure position (y--).
+     */
+    AbstractBlueprintIterator.Result decrement();
+
+    /**
+     * Decrement the structure with a certain skipCondition (jump over blocks that fulfill skipCondition).
+     * @param skipCondition the skipCondition.
+     * @return Result of decrement.
+     */
+    AbstractBlueprintIterator.Result decrement(TriPredicate<BlueprintPositionInfo, BlockPos, IStructureHandler> skipCondition);
+
+    /**
+     * Change the current progressPos. Used when loading progress.
+     *
+     * @param localPosition new progressPos.
+     */
+    void setProgressPos(BlockPos localPosition);
+
+    /**
+     * Get the blueprint info from the position.
+     * @param localPos the position.
+     * @return the info object.
+     */
+    BlueprintPositionInfo getBluePrintPositionInfo(BlockPos localPos);
+
+    /**
+     * Set the iterator to include entities.
+     */
+    void includeEntities();
+
+    /**
+     * Retrieve whether the iterator is taking entities into account as well
+     * @return whether the iterator is taking entities into account as well
+     */
+    boolean hasEntities();
+
+    /**
+     * Set the iterator to removal mode.
+     */
+    void setRemoving();
+
+    /**
+     * Retrieve whether we're removing blocks at the moment
+     * @return Whether we are removing blocks at the moment
+     */
+    boolean isRemoving();
+
+    /**
+     * Reset the progressPos.
+     */
+    void reset();
+
+    /**
+     * Get the progress pos of the iterator.
+     * @return the progress pos.
+     */
+    BlockPos getProgressPos();
+
+    /**
+     * Get the size of the blueprint which is iterated over
+     * @return the size
+     */
+    BlockPos getSize();
+}
