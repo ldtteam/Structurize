3f2d2187d2061f51b20181ff349c5158830e32ab
someaddons <someaddons@github.com>
2025-05-02 10:22:02 +0200

    Improve solid detection Add fallback to cobble

--- src/main/java/com/ldtteam/structurize/placement/handlers/placement/PlacementHandlers.java
+++ src/main/java/com/ldtteam/structurize/placement/handlers/placement/PlacementHandlers.java
@@ -246,13 +246,13 @@ public final class PlacementHandlers
             if (!BlockUtils.isAnySolid(world.getBlockState(pos.below())))
             {
                 BlockPos posBelow = pos;
-                BlockState supportBlockState = Blocks.DIRT.defaultBlockState();
+                BlockState supportBlockState = Blocks.DIRT instanceof Fallable ? Blocks.COBBLESTONE.defaultBlockState() : Blocks.DIRT.defaultBlockState();
                 for (int i = 0; i < 10; i++) // try up to ten blocks below for solid worldgen
                 {
                     posBelow = posBelow.below();
                     final boolean isFirstTest = i == 0;
                     final BlockState possibleSupport = BlockUtils.getWorldgenBlock(world, posBelow, bp -> isFirstTest ? blockState : null);
-                    if (possibleSupport != null && BlockUtils.isAnySolid(possibleSupport) && BlockUtils.canBlockFloatInAir(possibleSupport) && !canHandle(world,
+                    if (possibleSupport != null && BlockUtils.canBlockFloatInAir(possibleSupport) && !canHandle(world,
                         posBelow,
                         possibleSupport))
                     {
