edf2e9d145090cb1cb21ae1bc3effe4c78551c5e
Mirality <uecasm@users.noreply.github.com>
2025-06-30 11:35:12 +1200

    Use correct placement handler for removal

--- src/main/java/com/ldtteam/structurize/client/gui/WindowScan.java
+++ src/main/java/com/ldtteam/structurize/client/gui/WindowScan.java
@@ -475,22 +475,16 @@ public class WindowScan extends AbstractWindowSkeleton
             }
             else
             {
-                boolean handled = false;
-                for (final IPlacementHandler handler : PlacementHandlers.handlers)
+                final IPlacementHandler handler = PlacementHandlers.getHandler(world, here, blockState);
+                if (handler != null)
                 {
-                    if (handler.canHandle(world, here, blockState))
+                    final List<ItemStack> itemList = handler.getRequiredItems(world, here, blockState, tileEntity == null ? null : tileEntity.saveWithFullMetadata(world.registryAccess()), true);
+                    for (final ItemStack stack : itemList)
                     {
-                        final List<ItemStack> itemList = handler.getRequiredItems(world, here, blockState, tileEntity == null ? null : tileEntity.saveWithFullMetadata(world.registryAccess()), true);
-                        for (final ItemStack stack : itemList)
-                        {
-                            addNeededResource(stack, 1);
-                        }
-                        handled = true;
-                        break;
+                        addNeededResource(stack, 1);
                     }
                 }
-
-                if (!handled)
+                else
                 {
                     addNeededResource(BlockUtils.getItemStackFromBlockState(blockState), 1);
                 }
--- src/main/java/com/ldtteam/structurize/util/BlockUtils.java
+++ src/main/java/com/ldtteam/structurize/util/BlockUtils.java
@@ -460,7 +460,6 @@ public final class BlockUtils
         final BlockState blockState = world.getBlockState(position);
         final BlockEntity tileEntity = world.getBlockEntity(position);
         boolean isMatch = false;
-        boolean handled = false;
 
         if (block.getItem() == Items.AIR && blockState.isAir())
         {
@@ -468,22 +467,17 @@ public final class BlockUtils
         }
         else
         {
-            for (final IPlacementHandler handler : PlacementHandlers.handlers)
+            final IPlacementHandler handler = PlacementHandlers.getHandler(world, BlockPos.ZERO, blockState);
+            if (handler != null)
             {
-                if (handler.canHandle(world, BlockPos.ZERO, blockState))
+                final List<ItemStack> itemList =
+                  handler.getRequiredItems(world, position, blockState, tileEntity == null ? null : tileEntity.saveWithFullMetadata(world.registryAccess()), true);
+                if (!itemList.isEmpty() && ItemStackUtils.compareItemStacksIgnoreStackSize(itemList.get(0), block))
                 {
-                    final List<ItemStack> itemList =
-                      handler.getRequiredItems(world, position, blockState, tileEntity == null ? null : tileEntity.saveWithFullMetadata(world.registryAccess()), true);
-                    if (!itemList.isEmpty() && ItemStackUtils.compareItemStacksIgnoreStackSize(itemList.get(0), block))
-                    {
-                        isMatch = true;
-                    }
-                    handled = true;
-                    break;
+                    isMatch = true;
                 }
             }
-
-            if (!handled && ItemStackUtils.compareItemStacksIgnoreStackSize(BlockUtils.getItemStackFromBlockState(blockState), block))
+            else if (ItemStackUtils.compareItemStacksIgnoreStackSize(BlockUtils.getItemStackFromBlockState(blockState), block))
             {
                 isMatch = true;
             }
