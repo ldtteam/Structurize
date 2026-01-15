b3b40441e70f61b3dad3526bf7f5cf010dc4ff4f
Thom van den Akker <thodor20@gmail.com>
2026-01-15 15:14:01 +0100

    Update WindowTagTool.java

--- src/main/java/com/ldtteam/structurize/client/gui/WindowTagTool.java
+++ src/main/java/com/ldtteam/structurize/client/gui/WindowTagTool.java
@@ -126,12 +126,6 @@ public class WindowTagTool extends AbstractWindowSkeleton
             }
 
             @Override
-            public boolean shouldUpdate()
-            {
-                return false;
-            }
-
-            @Override
             public void updateElement(final int index, final Pane rowPane)
             {
                 final PositionWithTags positionWithTags = positionsList.get(index);
@@ -150,12 +144,6 @@ public class WindowTagTool extends AbstractWindowSkeleton
             public int getElementCount()
             {
                 return tagOptions.size();
-            }
-
-            @Override
-            public boolean shouldUpdate()
-            {
-                return false;
             }
 
             @Override
