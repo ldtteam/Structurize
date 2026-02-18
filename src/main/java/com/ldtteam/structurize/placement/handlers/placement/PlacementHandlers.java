d9424f78dfae3f4356f6a691f9a5080682abff1e
Raycoms <ray.neiheiser@gmail.com>
2026-02-18 14:27:48 +0100

    remove derp logging

--- src/main/java/com/ldtteam/structurize/placement/handlers/placement/PlacementHandlers.java
+++ src/main/java/com/ldtteam/structurize/placement/handlers/placement/PlacementHandlers.java
@@ -121,7 +121,6 @@ public final class PlacementHandlers
         final IPlacementHandler cached = handlerCache.get(block);
         if (cached != null)
         {
-            Log.getLogger().warn("Resolved: " + newState.toString() + " to " + cached.getClass().toString());
             return cached;
         }
 
@@ -129,7 +128,6 @@ public final class PlacementHandlers
         {
             if (placementHandler.canHandle(world, worldPos, newState))
             {
-                Log.getLogger().warn("Resolved: " + newState.toString() + " to " + placementHandler.getClass().toString());
                 handlerCache.put(block, placementHandler);
                 return placementHandler;
             }
