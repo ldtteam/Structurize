16a05cdb3524fae1662d8bab1f48b1d28f580c7a
Raycoms <ray.neiheiser@gmail.com>
2026-02-18 12:03:16 +0100

    Small fixes for rework

--- src/main/java/com/ldtteam/structurize/placement/handlers/placement/PlacementHandlers.java
+++ src/main/java/com/ldtteam/structurize/placement/handlers/placement/PlacementHandlers.java
@@ -1259,7 +1259,7 @@ public final class PlacementHandlers
             final Tuple<BlockEntity, CompoundTag> blockEntityData,
             final IPlacementContext placementContext)
         {
-            return worldState.equals(blueprintState) || BlockUtils.isGoodFloorBlock(worldState);
+            return worldState.equals(blueprintState) || (placementContext.fancyPlacement() && BlockUtils.isGoodFloorBlock(worldState));
         }
     }
 
