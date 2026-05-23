5cf63213f207db3c72c2d73a7c8e5edba60b45c0
Raycoms <ray.neiheiser@gmail.com>
2026-05-23 23:20:20 +0800

    fix placement with balm

--- src/main/java/com/ldtteam/structurize/placement/handlers/placement/PlacementHandlers.java
+++ src/main/java/com/ldtteam/structurize/placement/handlers/placement/PlacementHandlers.java
@@ -58,6 +58,7 @@ public final class PlacementHandlers
         handlers.add(new AirPlacementHandler());
         handlers.add(new SolidSubstitutionPlacementHandler());
         handlers.add(new SubstitutionPlacementHandler());
+        handlers.add(new BlockTagSubstitutionPlacementHandler());
         handlers.add(new BlackListedBlockPlacementHandler());
         handlers.add(new FluidSubstitutionPlacementHandler());
         handlers.add(new FirePlacementHandler());
@@ -75,7 +76,6 @@ public final class PlacementHandlers
         handlers.add(new DoBlockPlacementHandler());
         handlers.add(new DoDoorBlockPlacementHandler());
         handlers.add(new ContainerPlacementHandler());
-        handlers.add(new BlockTagSubstitutionPlacementHandler());
         handlers.add(new GeneralBlockPlacementHandler());
     }
 
